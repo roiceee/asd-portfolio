d7b380c59a73c81b128e729bde186d4597d521b6
roiceee <jroicealdeza@gmail.com>
2024-03-18 01:36:29 +0800

    Add HTTPS redirection in production environment

--- cms/app/Providers/AppServiceProvider.php
+++ cms/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
         //
     }
 }
